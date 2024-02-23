3c39cf1d15a5705a3c28e32283a7c7f58c288461
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-02-23 10:42:41 +0300

    explaining UI issues

---
+++ pasaj/pasaj.Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pasaj.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountRate { get; set; }
+        public int Stock { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public string ImageUrl { get; set; } = "https://ffo3gv1cf3ir.merlincdn.net/SiteAssets/pasaj/crop/cg/00LWMC/00LWMC-1/00LWMC-1_250x188.png?17735349480679";
+    }
+}
--- pasaj/pasaj.mvc/Controllers/HomeController.cs
+++ pasaj/pasaj.mvc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using pasaj.Entities;
 using pasaj.mvc.Models;
 using System.Diagnostics;
 
@@ -13,9 +14,32 @@ namespace pasaj.mvc.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            return View();
+            var products = new List<Product>
+            {
+                new(){ Id=1, Name="Ürün A", Description="Ürün A'nın Açıklaması", Price=2, DiscountRate=0.05m  },
+                new(){ Id=2, Name="Ürün B", Description="Ürün B'nın Açıklaması", Price=5, DiscountRate=0.05m  },
+                new(){ Id=3, Name="Ürün C", Description="Ürün C'nın Açıklaması", Price=7, DiscountRate=0.05m  },
+                new(){ Id=4, Name="Ürün D", Description="Ürün D'nın Açıklaması", Price=9, DiscountRate=0.05m  },
+                new(){ Id=5, Name="Ürün E", Description="Ürün D'nın Açıklaması", Price=9, DiscountRate=0.05m  },
+
+            };
+
+            //TODO 1: products'ı sayfala!
+
+            int pageSize = 2;
+            int total = products.Count;
+            int totalPages = (int)Math.Ceiling((decimal)total / pageSize);
+            ViewBag.Pages = totalPages;
+            ViewBag.Current = page;
+
+            var startPage = (page - 1) * pageSize;
+            var paginatedProducts = products.OrderBy(p => p.Id)
+                                            .Take(startPage..(startPage + pageSize))
+                                            .ToList();
+            //
+            return View(paginatedProducts);
         }
 
         public IActionResult Privacy()
